a12a416c3b3d84341b0f0e2a2642c3944738a7e8
Jasper Phelps <jtmaniatesselvin@g.harvard.edu>
2021-07-09 09:54:56 -0400

    Open image using PIL.Image, not pylab

--- examples/example_02.py
+++ examples/example_02.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-import pylab
+import numpy as np
+from PIL import Image
 
 import itfbarcode 
 
 
 if __name__ == '__main__':
-    im = pylab.imread('example_02_image.png')[:, :, 0]
+    im = np.array(Image.open('example_02_image.png'))[:, :, 0]
     barcode = itfbarcode.scanner.scan_image_y(im, 820, 1190, 570, 50)
     print("Barcode is: {}".format(barcode))
